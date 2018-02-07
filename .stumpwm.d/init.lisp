a0d9a746324e0e72e2b6de1a6b1efd70b0acd92b
t-sin <shinichi.tanaka45@gmail.com>
2018-02-07 10:38:24 +0900

    Fundamental configurations for stumpwm

--- .stumpwm.d/init.lisp
+++ .stumpwm.d/init.lisp
@@ -1,5 +1,19 @@
 (in-package :stumpwm)
 
+;;;; definition
+(defcommand run-gterm ()
+  nil
+  (run-shell-command "gnome-terminal"))
+
+;;;; key bindings
 (set-prefix-key (kbd "C-["))
 
+(undefine-key *root-map* (kbd "t"))
+(define-key *root-map* (kbd "t") "run-gterm")
+
+;;;; initializing
+(run-shell-command "gksudo --description 'running xkeysnail' -- xkeysnail ~/.xkeysnail.config.py --devices /dev/input/event3")
 (mode-line)
+
+
+
